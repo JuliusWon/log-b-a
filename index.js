8b96273f8e5265452cb168545d829799d54bcd66
JuliusWon <julius.won@icsd.k12.ny.us>
2022-06-11 23:23:00 -0400

    Added framework and complex number grapher

---
+++ index.js
@@ -0,0 +1,8 @@
+// int num=1;
+var urls = ["https://nourek.itch.io/","https://github.com/JuliusWon/","https://sidequestvr.com/app/1613/laser-arcade-vr","https://www.youtube.com/channel/UCpGiOJXgVeUbdEncJF9lNmw"]
+function OpenRes(number){
+	window.open(urls[number])
+}
+function OpenLatest(){
+	window.open("./1/page.html")
+}
